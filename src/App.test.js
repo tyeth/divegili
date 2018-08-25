2d023c8b1a75ad724e4feeb08997abdf5bd12005
Tyeth Gundry <tyethgundry@googlemail.com>
2018-08-25 15:24:13 +0100

    Added tests for header and areas, areas still to implement

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,19 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('has correct heading', ()=> {
+	const div = document.createElement('div');
+	ReactDOM.render(<App />,div);
+	var h1s =  div.getElementsByTagName("H1");
+	expect(h1s.length).toEqual(1);
+	expect(h1s[0].textContent).toEqual("Dive Gili");
+});
+
+it('has a list of areas', () => {
+	const div = document.createElement('div');
+	ReactDOM.render(<App />,div);
+	var areas = div.getElementsByClassName("diving-area")
+	expect(areas!=null).toEqual(true);
+	expect(areas.length>0).toEqual(true);
+});
